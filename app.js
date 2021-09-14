3bae95e03efc39d05f544063cc2e5c50a874a378
MlhSlckr <selcukermelih@gmail.com>
2021-09-14 15:51:24 +0300

    animation

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const fass = document.querySelectorAll('.fas');
-
+/*
 fass.forEach(fas => {
   fas.addEventListener('click', (e) => {
     const article = e.currentTarget.parentElement.parentElement;
     article.classList.toggle('show')
     fas.classList.toggle('rotate')
+  })
+})
+*/
+
+fass.forEach(fas => {
+  fas.addEventListener('click', (e) => {
+    const article = e.currentTarget.parentElement.parentElement;
+    article.classList.toggle('show');
+    fas.classList.toggle('rotate');
   })
 })
